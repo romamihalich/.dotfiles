2acb8e55abd9657c57b93bf7e1d99274bf9e4ad2
romamihalich <miheev.roma90@mail.ru>
2021-12-21 20:02:13 +0300

    Fix borders

--- nvim/.config/nvim/lua/romamihalich/nvim-lsp-installer.lua
+++ nvim/.config/nvim/lua/romamihalich/nvim-lsp-installer.lua
@@ -7,14 +7,14 @@ vim.cmd [[autocmd ColorScheme * highlight NormalFloat guibg=#1f2335]]
 vim.cmd [[autocmd ColorScheme * highlight FloatBorder guifg=white guibg=#1f2335]]
 
 local border = {
-      {" ", "FloatBorder"},
-      {"▔", "FloatBorder"},
-      {" ", "FloatBorder"},
-      {" ", "FloatBorder"},
-      {" ", "FloatBorder"},
-      {"▁", "FloatBorder"},
-      {" ", "FloatBorder"},
-      {" ", "FloatBorder"},
+      {"╭", "FloatBorder"},
+      {"─", "FloatBorder"},
+      {"╮", "FloatBorder"},
+      {"│", "FloatBorder"},
+      {"╯", "FloatBorder"},
+      {"─", "FloatBorder"},
+      {"╰", "FloatBorder"},
+      {"│", "FloatBorder"},
 }
 
 local on_attach = function(client, bufnr)
