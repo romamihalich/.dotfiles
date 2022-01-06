b43fd75f8a8d89c026ef816ca730467240679156
romamihalich <miheev.roma90@mail.ru>
2022-01-06 20:24:51 +0300

    Add debugpy

--- nvim/.config/nvim/lua/romamihalich/nvim-dap.lua
+++ nvim/.config/nvim/lua/romamihalich/nvim-dap.lua
@@ -3,9 +3,11 @@ if not status_ok then
     return
 end
 
+local debuggers_path = os.getenv('HOME') .. '/.local/share/nvim/debuggers'
+
 dap.adapters.netcoredbg = {
   type = 'executable',
-  command = os.getenv('HOME') .. '/.local/share/nvim/debuggers/netcoredbg/netcoredbg',
+  command = debuggers_path .. '/netcoredbg/netcoredbg',
   args = {'--interpreter=vscode'}
 }
 
@@ -17,6 +19,38 @@ dap.configurations.cs = {
     program = function()
         return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
     end,
+  },
+}
+
+dap.adapters.python = {
+  type = 'executable';
+  command = debuggers_path .. '/debugpy/bin/python';
+  args = { '-m', 'debugpy.adapter' };
+}
+
+dap.configurations.python = {
+  {
+    -- The first three options are required by nvim-dap
+    type = 'python'; -- the type here established the link to the adapter definition: `dap.adapters.python`
+    request = 'launch';
+    name = "Launch file";
+
+    -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+    program = "${file}"; -- This configuration will launch the current file if used.
+    pythonPath = function()
+      -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+      -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+      -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+      local cwd = vim.fn.getcwd()
+      if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+        return cwd .. '/venv/bin/python'
+      elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+        return cwd .. '/.venv/bin/python'
+      else
+        return '/usr/bin/python'
+      end
+    end;
   },
 }
 
