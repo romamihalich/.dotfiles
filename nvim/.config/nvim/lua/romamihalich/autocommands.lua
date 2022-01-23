030b8255474639fc5a00890f7b2ac05aa1d4629a
romamihalich <miheev.roma90@mail.ru>
2022-01-23 13:32:05 +0300

    latex autocmd

--- nvim/.config/nvim/lua/romamihalich/autocommands.lua
+++ nvim/.config/nvim/lua/romamihalich/autocommands.lua
@@ -5,6 +5,7 @@ vim.cmd [[
            autocmd VimEnter * :silent exec "!kill -s SIGWINCH $PPID"
            autocmd BufWritePost *Xresources,*Xdefaults !xrdb %
            autocmd BufWritePost *sxhkdrc !pkill -USR1 -x sxhkd
+           autocmd BufWritePost *.tex silent! execute "!latexmk -pdf '%' >/dev/null 2>&1 &" | redraw!
     augroup END
 
     augroup yank_highlight
