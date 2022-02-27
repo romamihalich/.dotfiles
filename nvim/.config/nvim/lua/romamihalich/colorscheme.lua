b9d37386dddc5a78b2d1d0fea3c08d3f19eba057
romamihalich <miheev.roma90@mail.ru>
2022-02-27 17:10:46 +0300

    change search colors

--- nvim/.config/nvim/lua/romamihalich/colorscheme.lua
+++ nvim/.config/nvim/lua/romamihalich/colorscheme.lua
@@ -1,4 +1,13 @@
 require('onedark').setup {
     style = 'darker',
+    colors = {
+        search_blue   = "#5e81ac",
+        search_orange = "#613214",
+    },
+    highlights = {
+		Search     = {fg = "#ffffff", bg = "$search_blue", },
+		IncSearch  = {fg = "#ffffff", bg = "$search_blue", },
+		Substitute = {fg = "#ffffff", bg = "$search_orange", },
+    }
 }
 require('onedark').load()
