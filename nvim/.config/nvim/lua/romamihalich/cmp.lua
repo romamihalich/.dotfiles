6556739b1eed89dd7b02e3745afb454dda0bdef2
romamihalich <miheev.roma90@mail.ru>
2022-04-23 15:37:37 +0300

    cmp: clear config

--- nvim/.config/nvim/lua/romamihalich/cmp.lua
+++ nvim/.config/nvim/lua/romamihalich/cmp.lua
@@ -15,11 +15,6 @@ end
 
 require("luasnip/loaders/from_vscode").lazy_load()
 
-local check_backspace = function()
-  local col = vim.fn.col "." - 1
-  return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
-end
-
 cmp.setup({
   --completion = {
   --    completeopt = 'menu,menuone,noinsert'
@@ -47,34 +42,6 @@ cmp.setup({
       "i",
       "s",
     }),
-    -- ["<Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expandable() then
-    --     luasnip.expand()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     luasnip.expand_or_jump()
-    --   elseif check_backspace() then
-    --     fallback()
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
-    -- ["<S-Tab>"] = cmp.mapping(function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end, {
-    --   "i",
-    --   "s",
-    -- }),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
   },
@@ -106,9 +73,6 @@ cmp.setup({
     { name = 'luasnip', keyword_length = 3 },
     --{ name = 'buffer' },
     { name = 'path'}
-  },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
   },
 })
 
