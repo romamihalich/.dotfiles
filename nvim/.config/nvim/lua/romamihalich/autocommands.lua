e1f690d95f3096e440678aa3efb5a8b526a3c447
romamihalich <miheev.roma90@mail.ru>
2022-04-25 19:54:39 +0300

    nvim: use lua autocmds

--- nvim/.config/nvim/lua/romamihalich/autocommands.lua
+++ nvim/.config/nvim/lua/romamihalich/autocommands.lua
@@ -1,23 +1,41 @@
+local group = vim.api.nvim_create_augroup("kek", { clear = true })
+vim.api.nvim_clear_autocmds({ group = group })
+local autocmd = function(event, opts)
+    opts.group = group
+    vim.api.nvim_create_autocmd(event, opts)
+end
+
+autocmd("TermOpen", { command = "startinsert" })
+
+autocmd("VimEnter", { command = ':silent exec "!kill -s SIGWINCH $PPID"'})
+
+autocmd("BufWritePost", {
+    pattern = { "*Xresources", "*Xdefaults" },
+    command = "!xrdb %"
+})
+
+autocmd("BufWritePost", {
+    pattern = "*sxhkdrc",
+    command = "!pkill -USR1 -x sxhkd"
+})
+
+autocmd("BufWritePost", {
+    pattern = "*.tex",
+    command = 'silent! execute "!latexmk -pdf \'%\' >/dev/null 2>&1 &" | redraw!'
+})
+
+autocmd("FileType", {
+    pattern = "http",
+    callback = function ()
+        vim.keymap.set('n', '<CR>', require'rest-nvim'.run, { silent = true, buffer = true })
+    end,
+})
+
 vim.cmd [[
-    augroup kek
-        autocmd!
-           autocmd TermOpen * startinsert
-           autocmd VimEnter * :silent exec "!kill -s SIGWINCH $PPID"
-
-           autocmd BufWritePost *Xresources,*Xdefaults !xrdb %
-           autocmd BufWritePost *sxhkdrc !pkill -USR1 -x sxhkd
-           autocmd BufWritePost *.tex silent! execute "!latexmk -pdf '%' >/dev/null 2>&1 &" | redraw!
-    augroup END
-
-    augroup filetype_mappings
-        autocmd!
-           autocmd FileType http nnoremap <silent> <buffer> <CR> :lua require'rest-nvim'.run()<CR>
-    augroup END
-
     augroup yank_highlight
       autocmd!
-      if exists('##TextYankPost')
-        autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()
-      endif
+        if exists('##TextYankPost')
+          autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank()
+        endif
     augroup END
 ]]
