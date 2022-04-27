5138a5b2c68e66ccd8676c109853dd5e7393a0d9
romamihalich <miheev.roma90@mail.ru>
2022-04-27 21:53:59 +0300

    move which_key keymaps to 'keymaps.lua'

--- nvim/.config/nvim/lua/romamihalich/keymaps.lua
+++ nvim/.config/nvim/lua/romamihalich/keymaps.lua
@@ -1,3 +1,5 @@
+local M = {}
+
 local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
@@ -49,3 +51,91 @@ keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", opts)
 keymap("n", "<F12>", ":lua require'dap'.step_out()<CR>", opts)
 -- nnoremap <silent> <leader>B :lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>
 -- nnoremap <silent> <leader>lp :lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>
+
+local last_command = " "
+function run_shell_command()
+    vim.ui.input({ prompt = "command:", default = last_command }, function(inp)
+        if inp then
+            last_command = inp
+            vim.cmd('sp +term\\ ' .. last_command:gsub(" ", "\\ "))
+        end
+    end)
+end
+
+function dap_close()
+    require'dap'.close()
+    require'dapui'.close()
+end
+
+M.which_key_visual = {
+    ["<leader>"] = {
+      c = {
+        name = "+code",
+        a = { ":Telescope lsp_range_code_actions theme=cursor<CR>", "Code actions"}
+      }
+    }
+}
+
+M.which_key_normal = {
+  ["<leader>"] = {
+    e = { "<cmd>NvimTreeToggle<CR>", "Explorer toggle" },
+    q = { "<cmd>bd!<CR>", "Close current buffer" },
+    r = { "<cmd>lua run_shell_command()<CR>", "Run a shell command" },
+    H = { "<cmd>cd %:p:h | pwd<CR>", "Cd here" },
+    f = {
+      name = "+find",
+      f = { "<cmd>Telescope find_files<CR>", "Files" },
+      r = { "<cmd>Telescope oldfiles<CR>", "Recent files" },
+      g = { "<cmd>Telescope git_files<CR>", "Git files" },
+      l = { "<cmd>Telescope live_grep<CR>", "Live grep" },
+      d = { "<cmd>Telescope diagnostics<CR>", "Diagnostics" },
+      s = { "<cmd>Telescope lsp_workspace_symbols<CR>", "Symbols" },
+      -- cwd_to_path=true
+      -- grouped=true
+      -- default_selection_index=2
+      -- files=bool
+      o = { "<cmd>Telescope file_browser cwd_to_path=true grouped=true<CR>", "File browser" }
+    },
+    c = {
+      name = "+code",
+      a = { "<cmd>Telescope lsp_code_actions theme=cursor<CR>", "Actions" },
+      f = { "<cmd>lua vim.lsp.buf.formatting()<CR>", "Formatting" },
+      r = { "<cmd>lua vim.lsp.buf.rename()<CR>", "Rename" },
+      l = { "<cmd>lua vim.diagnostic.open_float()<CR>", "Line diagnostics" },
+      j = { "<cmd>lua vim.diagnostic.goto_next()<CR>", "Next diagnostic" },
+      k = { "<cmd>lua vim.diagnostic.goto_prev()<CR>", "Prev diagnostic" },
+    },
+    g = {
+      name = "+GTD",
+      n = {"<cmd>GtdNew<CR>", "New" },
+      r = {"<cmd>GtdReview<CR>", "Review" }
+    },
+    o = {
+      name = "+open",
+      t = { "<cmd>!$TERMINAL --working-directory \"$PWD\"&<CR><CR>", "Terminal" },
+      c = { "<cmd>term cht.sh<CR>", "Cheet sheet" }
+    },
+    h = {
+      name = "+harpoon",
+      a = { "<cmd> lua require'harpoon.mark'.add_file()<CR>", "Add file" },
+      t = { "<cmd> lua require'harpoon.ui'.toggle_quick_menu()<CR>", "Toggle menu" },
+      h = { "<cmd> lua require'harpoon.ui'.nav_file(1)<CR>", "Nav file 1" },
+      j = { "<cmd> lua require'harpoon.ui'.nav_file(2)<CR>", "Nav file 2" },
+      k = { "<cmd> lua require'harpoon.ui'.nav_file(3)<CR>", "Nav file 3" },
+    },
+    d = {
+      name = "+debug",
+      p = { "<cmd> lua require'dap'.pause()<CR>", "Pause" },
+      q = { "<cmd> lua dap_close()<CR>", "Close" },
+      h = { "<cmd> lua require'dap.ui.widgets'.hover()<CR>", "Hover" },
+      e = { "<cmd> lua require'dapui'.toggle()<CR>", "Toggle sidebar" },
+    },
+  },
+  g = {
+    d = { "<cmd>Telescope lsp_definitions<CR>", "Go to definition" },
+    r = { "<cmd>Telescope lsp_references<CR>", "Go to references" },
+    s = { "<cmd>lua vim.lsp.buf.signature_help()<CR>", "Signature help" }
+  }
+}
+
+return M
