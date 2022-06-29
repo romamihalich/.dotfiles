2d4afaadeb3863ac88a5ccd20ad10b16f35db03c
romamihalich <miheev.roma90@mail.ru>
2022-06-29 14:27:31 +0300

    quickfix colors change

--- nvim/.config/nvim/lua/romamihalich/colorscheme.lua
+++ nvim/.config/nvim/lua/romamihalich/colorscheme.lua
@@ -9,12 +9,16 @@ onedark.setup {
     colors = {
         search_blue   = "#5e81ac",
         search_orange = "#613214",
+        -- my_blue = "#61AFEF",
+        my_blue = "#264F78",
+        dark_gray = "#2a2f3e",
     },
     highlights = {
-		Search     = {fg = "#ffffff", bg = "$search_blue", },
+		Search     = {fg = "#ffffff", bg = "$my_blue", },
 		IncSearch  = {fg = "#ffffff", bg = "$search_blue", },
 		Substitute = {fg = "#ffffff", bg = "$search_orange", },
-        TSPunctBracket = {fg = "#abb2bf"}
+        TSPunctBracket = {fg = "#abb2bf"},
+        QuickFixLine = { bg = "$dark_gray", fmt = "bold", fg = "none" },
     }
 }
 onedark.load()
