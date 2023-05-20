589f90b9843034e8fc009044cf8e7ce7888bd1ad
romamihalich <miheev.roma90@mail.ru>
2023-05-20 10:57:05 +0300

    add oil.nvim

---
+++ nvim/.config/nvim/lua/romamihalich/oil.lua
@@ -0,0 +1,12 @@
+require('oil').setup({
+    columns = {
+        "icon",
+        -- "permissions",
+        -- "size",
+        -- "mtime",
+    },
+    view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+  },
+})
--- nvim/.config/nvim/lua/romamihalich/plugins.lua
+++ nvim/.config/nvim/lua/romamihalich/plugins.lua
@@ -155,6 +155,8 @@ return packer.startup(function(use)
 
     use { "folke/trouble.nvim", config = function() require("trouble").setup { } end }
 
+    use { 'stevearc/oil.nvim', config = function() require'romamihalich.oil' end }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
